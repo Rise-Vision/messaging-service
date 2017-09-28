7588c07246b32b455f25a79a61f1a14f1f86955e
rodrigopavezi <rodrigopavezi@gmail.com>
2017-09-27 23:12:00 -0300

    Trying to fix route again

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const Primus = require('primus'),
-      express = require('express');
       http = require('http');
       port = 80;
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const podname = process.env.podname;
 
 const primus = new Primus(server, {transformer: 'uws', pathname: '/messaging/primus'});
 
 primus.on('connection', (spark) => {
   spark.write(`Messaging Service WebSocket Connected: ${podname}`);
-});
-
-app.get('/messaging', function(req, res) {
-  res.send(`Messaging Service: ${podname}`);
 });
 
 server.listen(port, (err) => {
