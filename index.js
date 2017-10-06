7ff78f685d7b05db2316f953d5a0caa40668c40d
Stuart Lees <stuartlees@gmail.com>
2017-10-06 12:19:29 -0400

    CCI to deploy to GKE

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const Primus = require('primus'),
       port = 80;
 const app = express();
 const server = http.createServer(app);
+const pkg = require("./package.json");
 const podname = process.env.podname;
 
 const primus = new Primus(server, {transformer: 'uws', pathname: 'messaging/primus'});
 
 primus.on('connection', (spark) => {
-  spark.write(`Messaging Service WebSocket Connected: ${podname}`);
+  spark.write(`Messaging Service WebSocket Connected: ${podname} ${pkg.version}`);
 });
 
 app.get('/messaging', function(req, res) {
-  res.send(`Messaging Service: ${podname}`);
+  res.send(`Messaging Service: ${podname} ${pkg.version}`);
 });
 
 server.listen(port, (err) => {
