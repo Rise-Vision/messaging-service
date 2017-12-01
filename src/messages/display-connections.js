86ddd1a082c9fb630c1cf32893e0f7ee6e1b990b
Tyler Johnson <mail@tyler-johnson.ca>
2017-12-01 11:24:13 -0500

    Use the appropriate prototype functions for Map

--- src/messages/display-connections.js
+++ src/messages/display-connections.js
@@ -4,16 +4,16 @@ const logger = require("../logger.js");
 module.exports = {
   put(spark) {
     if (!spark || !spark.query) {return;}
-    sparks[spark.query.displayId] = spark;
+    sparks.set(spark.query.displayId, spark);
     logger.log(`Added spark for ${spark.query.displayId}`);
   },
   remove(spark) {
-    logger.log(`Removed spark for ${spark.query.displayId}`);
     if (!spark || !spark.query) {return;}
-    Reflect.deleteProperty(sparks, spark.query.displayId);
+    sparks.delete(spark.query.displayId);
+    logger.log(`Removed spark for ${spark.query.displayId}`);
   },
   sendMessage(displayId, msg) {
-    const spark = sparks[displayId];
+    const spark = sparks.get(displayId);
     if (!spark) {return;}
 
     spark.write(msg);
