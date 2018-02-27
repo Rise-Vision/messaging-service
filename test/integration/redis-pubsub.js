b4cbf835799e8384d42fc7ad124d26c15019adbd
santiagonoguez <santiago@nul-unu.com>
2018-02-27 14:52:07 -0600

    add tests to restart / reboot message forward

--- test/integration/redis-pubsub.js
+++ test/integration/redis-pubsub.js
@@ -11,6 +11,7 @@ const redisHost = "127.0.0.1";
 const channel = "pubsub-update";
 const displayConnections = require("../../src/messages/display-connections");
 const fileStatusUpdate = require("../../src/redis-pubsub/file-status-update");
+const logger = require("../../src/logger");
 
 describe("Pubsub : Integration", ()=>{
   let msServer = null;
@@ -30,9 +31,15 @@ describe("Pubsub : Integration", ()=>{
 
   describe("With a local running redis server", ()=>{
     describe("and the messaging service under test running, it...", ()=>{
+      let restartReboot = null;
+      let pubsub = null;
+
       before(()=>{
         msServer = require("../../index.js");
         msServer.dropSocketsAfterTimeMS(reasonableResponseTime);
+
+        restartReboot = require("../../src/messages/restart-reboot");
+        pubsub = require("../../src/redis-pubsub");
       });
 
       it("receives POST update from pubsub connector, processes update, and shares to other pods", ()=>{
@@ -130,6 +137,57 @@ describe("Pubsub : Integration", ()=>{
           simple.restore(displayConnections, "sendMessage");
         });
       });
+
+      it("forwards reboot messages", () => {
+        simple.mock(logger, "log").returnWith();
+        simple.mock(displayConnections, "sendMessage").returnWith();
+        simple.mock(pubsub, "publish").returnWith();
+
+        restartReboot.forwardRebootMessage('ABC124');
+
+        assert(displayConnections.sendMessage.callCount, 1);
+        assert.deepEqual(displayConnections.sendMessage.lastCall.args, [
+          'ABC124', {
+            msg: 'reboot-request', displayId: 'ABC124'
+          }
+        ]);
+
+        assert(pubsub.publish.callCount, 1);
+        const message = pubsub.publish.lastCall.args[0]
+        assert.deepEqual(JSON.parse(message), {
+            msg: 'reboot-request', displayId: 'ABC124', podname
+        });
+
+        simple.restore(logger, "log");
+        simple.restore(displayConnections, "sendMessage");
+        simple.restore(pubsub, "publish");
+      });
+
+      it("forwards restart messages", () => {
+        simple.mock(logger, "log").returnWith();
+        simple.mock(displayConnections, "sendMessage").returnWith();
+        simple.mock(pubsub, "publish").returnWith();
+
+        restartReboot.forwardRestartMessage('ABC124');
+
+        assert(displayConnections.sendMessage.callCount, 1);
+        assert.deepEqual(displayConnections.sendMessage.lastCall.args, [
+          'ABC124', {
+            msg: 'restart-request', displayId: 'ABC124'
+          }
+        ]);
+
+        assert(pubsub.publish.callCount, 1);
+        const message = pubsub.publish.lastCall.args[0]
+        assert.deepEqual(JSON.parse(message), {
+            msg: 'restart-request', displayId: 'ABC124', podname
+        });
+
+        simple.restore(logger, "log");
+        simple.restore(displayConnections, "sendMessage");
+        simple.restore(pubsub, "publish");
+      });
+
     });
   });
 
