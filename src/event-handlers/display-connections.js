f38837e8902b6ef6c5493e632e9517b2d4c6e461
Tyler Johnson <mail@tyler-johnson.ca>
2018-03-06 14:56:07 -0500

    Send connection event for apps

--- src/event-handlers/display-connections.js
+++ src/event-handlers/display-connections.js
@@ -5,7 +5,9 @@ const logger = require("../logger.js");
 module.exports = {
   put(spark) {
     if (!spark || !spark.query) {return;}
-    const displayId = spark.query.displayId;
+    const displayId = spark.query.displayId === "apps" ?
+      spark.id :
+      spark.query.displayId;
 
     sparks.set(displayId, spark);
     db.connections.setConnected(displayId).catch(console.error);
@@ -14,7 +16,9 @@ module.exports = {
   },
   remove(spark) {
     if (!spark || !spark.query) {return;}
-    const displayId = spark.query.displayId;
+    const displayId = spark.query.displayId === "apps" ?
+      spark.id :
+      spark.query.displayId;
 
     sparks.delete(displayId);
     db.connections.setDisconnected(displayId).catch(console.error);
