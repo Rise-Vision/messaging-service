2d8132f5bbc116cf6c290272a5d26d459b6a4f50
santiagonoguez <santiago@nul-unu.com>
2018-03-26 08:25:03 -0600

    get full watchlist for a display

--- test/integration/db-api.js
+++ test/integration/db-api.js
@@ -1,4 +1,6 @@
 /* eslint-env mocha */
+/* eslint-disable function-paren-newline */
+
 const assert = require("assert");
 const simple = require("simple-mock");
 
@@ -16,19 +18,44 @@ describe("DB API : Integration", ()=>{
 
   afterEach(() => simple.restore());
 
-  it("updates and retrieves last changed value", ()=>{
+  describe("watchList", () => {
     const fakeTimestamp = 12345;
 
-    simple.mock(Date, "now").returnWith(fakeTimestamp);
+    beforeEach(() => {
+      simple.mock(Date, "now").returnWith(fakeTimestamp);
+    })
 
-    return dbApi.watchList.updateLastChanged("ABC124")
-    .then(() => dbApi.watchList.lastChanged("ABC124"))
-    .then(lastChanged => assert.equal(lastChanged, fakeTimestamp));
-  });
+    it("updates and retrieves last changed value", () => {
+      return dbApi.watchList.updateLastChanged("ABC124")
+      .then(() => dbApi.watchList.lastChanged("ABC124"))
+      .then(lastChanged => assert.equal(lastChanged, fakeTimestamp));
+    });
 
-  it("retrieves last changed value even if it was not set before", ()=>{
-    return dbApi.watchList.lastChanged("ABC124")
-    .then(lastChanged => assert.equal(lastChanged, '0'));
+    it("retrieves last changed value even if it was not set before", () => {
+      return dbApi.watchList.lastChanged("ABC124")
+      .then(lastChanged => assert.equal(lastChanged, '0'));
+    });
+
+    it("gets the full watchlist for a display", () => {
+      const filePaths = ["bucket/file1", "bucket/file2", "bucket/file3"];
+
+      return Promise.all(
+        filePaths.map(filePath => ({
+          filePath, version: "1", displayId: "ABC124"
+        }))
+        .map(dbApi.watchList.put)
+      )
+      .then(() => dbApi.watchList.get("ABC124"))
+      .then(watchlist => {
+        assert.deepEqual(watchlist, {
+          "bucket/file1": "1",
+          "bucket/file2": "1",
+          "bucket/file3": "1"
+        });
+      })
+      .then(() => dbApi.watchList.lastChanged("ABC124"))
+      .then(lastChanged => assert.equal(lastChanged, fakeTimestamp));
+    });
   });
 
 });
