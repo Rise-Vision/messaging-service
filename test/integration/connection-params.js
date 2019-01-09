4d299eed6ca101e3f1e3d0a273b5dd2e8895c29a
André Luiz Cardoso <andrecardoso@gmail.com>
2019-01-09 10:05:00 -0500

    Fix unit tests

--- test/integration/connection-params.js
+++ test/integration/connection-params.js
@@ -17,7 +17,7 @@ describe("MS Connection : Integration", ()=>{
   });
 
   describe("With the messaging server under test running it...", ()=>{
-    it("prevents connections without a displayId", ()=>{
+    it("accepts connections without a displayId", ()=>{
       const displayId = "testId";
       const machineId = "testMachineId";
       const msUrl = `${msEndpoint}?XXXXdisplayId=${displayId}&machineId=${machineId}`;
@@ -30,12 +30,12 @@ describe("MS Connection : Integration", ()=>{
       }))(msUrl);
 
       return new Promise((res, rej)=>{
-        ms.on("open", ()=>rej(Error("Should not have allowed the connection")));
-        ms.on("error", (err)=>{console.error(err.message); res()});
+        ms.on("open", res);
+        ms.on("error", ()=>rej(Error("Should not have prevented the connection")));
       });
     });
 
-    it("prevents connections without a machineId", ()=>{
+    it("accepts connections without a machineId", ()=>{
       const displayId = "testId";
       const machineId = "testMachineId";
       const msUrl = `${msEndpoint}?displayId=${displayId}&XXXXXXmachineId=${machineId}`;
@@ -48,49 +48,13 @@ describe("MS Connection : Integration", ()=>{
       }))(msUrl);
 
       return new Promise((res, rej)=>{
-        ms.on("open", ()=>rej(Error("Should not have allowed the connection")));
-        ms.on("error", (err)=>{console.error(err.message); res()});
+        ms.on("open", res);
+        ms.on("error", ()=>rej(Error("Should not have prevented the connection")));
       });
     });
 
-    it("prevents connections with string undefined as a displayId", ()=>{
+    it("prevents connections with string undefined as a displayId and machineId", ()=>{
       const displayId = "undefined";
-      const machineId = "testMachineId";
-      const msUrl = `${msEndpoint}?displayId=${displayId}&machineId=${machineId}`;
-      console.log(`Connecting to websocket with ${msUrl}`);
-
-      const ms = new (Primus.createSocket({
-        transformer: "websockets",
-        pathname: "messaging/primus/",
-        reconnect: {retries: 0}
-      }))(msUrl);
-
-      return new Promise((res, rej)=>{
-        ms.on("open", ()=>rej(Error("Should not have allowed the connection")));
-        ms.on("error", (err)=>{console.error(err.message); res()});
-      });
-    });
-
-    it("prevents connections with string null as a displayId", ()=>{
-      const displayId = "null";
-      const machineId = "testMachineId";
-      const msUrl = `${msEndpoint}?displayId=${displayId}&machineId=${machineId}`;
-      console.log(`Connecting to websocket with ${msUrl}`);
-
-      const ms = new (Primus.createSocket({
-        transformer: "websockets",
-        pathname: "messaging/primus/",
-        reconnect: {retries: 0}
-      }))(msUrl);
-
-      return new Promise((res, rej)=>{
-        ms.on("open", ()=>rej(Error("Should not have allowed the connection")));
-        ms.on("error", (err)=>{console.error(err.message); res()});
-      });
-    });
-
-    it("prevents connections with string undefined as a machineId", ()=>{
-      const displayId = "testId";
       const machineId = "undefined";
       const msUrl = `${msEndpoint}?displayId=${displayId}&machineId=${machineId}`;
       console.log(`Connecting to websocket with ${msUrl}`);
@@ -107,8 +71,8 @@ describe("MS Connection : Integration", ()=>{
       });
     });
 
-    it("prevents connections with string null as a machineId", ()=>{
-      const displayId = "testId";
+    it("prevents connections with string null as a displayId and machineId", ()=>{
+      const displayId = "null";
       const machineId = "null";
       const msUrl = `${msEndpoint}?displayId=${displayId}&machineId=${machineId}`;
       console.log(`Connecting to websocket with ${msUrl}`);
